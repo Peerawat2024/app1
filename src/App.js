87d8e097e818c7a60901e431221bfb27d009d026
Peerawat2024 <s6403051623101@email.kmutnb.ac.th>
2024-08-05 10:27:50 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Style.css'
+
+function App(){
+  return(
+    <>
+    <h2>DeveloperThai.com</h2>
+    <div className='title'>React & React native</div>
+    </>
+  )
+}
+export default App;
